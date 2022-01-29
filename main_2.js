28c7f699dfebf6182f6f06d66bbb45e1e5c2916d
apai230910 <91840707+apai230910@users.noreply.github.com>
2022-01-29 20:55:34 +0530

    Add files via upload

---
+++ main_2.js
@@ -0,0 +1,6 @@
+// player1_name = localStorage.getItem(player_1_name);
+// player2_name = localStorage.getItem(player_2_name);
+
+// document.getElementById("player_1_name").innerHTML = player1_name + " : ";
+// document.getElementById("player_2_name").innerHTML = player2_name + " : ";
+
